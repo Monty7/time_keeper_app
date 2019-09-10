a834bd3d63f117876200499c862be498c2b8a4b5
github username <github email address>
2019-09-10 17:28:30 -0500

    Added buttons to each date on calendar and added fetch to button event click listener

--- time_keeper-frontend/src/script.js
+++ time_keeper-frontend/src/script.js
@@ -1,2 +1,28 @@
-const BASE_URL = "http://localhost:3000/times";
+const BASE_URL = "http://localhost:3000/user_times";
+
+const calendarContainer = document.querySelector('.calendar');
+//const addTimeBtn = document.querySelectorAll('.addTime');addTimeBtn.addEventListener('click', function(e){
+calendarContainer.addEventListener('click', function(e){
+
+    e.preventDefault();
+    if(e.target.tagName === "BUTTON"){
+        let captured_date = e.target.parentElement.children[0].innerText;
+        let clocked_in = e.target.parentElement.children[2].value;
+        let clocked_out = e.target.parentElement.children[4].value;
+        fetch(BASE_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"           
+            },
+            body: JSON.stringify({clock_in: clocked_in, clock_out: clocked_out})
+        })
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            console.log(data);
+        })
+    }
+})
 
